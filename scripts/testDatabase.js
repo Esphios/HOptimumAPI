919dc9d68231e5f5bb64123a6af2d2a0179a68e6
EduardoRdeL <eduardo.lima@tars.com.br>
2022-10-12 18:40:32 -0300

    Scripts refactorated

--- scripts/testDatabase.js
+++ scripts/testDatabase.js
@@ -1,90 +1,18 @@
 const mongoose = require("mongoose");
 const db = require("../models");
+const {
+  createFuncionario,
+  createServico,
+  addFuncionarioServico,
+  getFuncionarioWithPopulate,
+  getServicoWithPopulate,
+  createQuarto,
+  createCartaoChave,
+  pushCartaoChaveToFunc,
+  dropCollection
+} = require("./utilsDB");
 
-const createFuncionario = function (funcionario) {
-  return db.Funcionario.create(funcionario).then((docFuncionario) => {
-    console.log("\n>> Created Funcionario:\n", docFuncionario);
-    return docFuncionario;
-  });
-};
-
-const createServico = function (servico) {
-  return db.Servico.create(servico).then((docServico) => {
-    console.log("\n>> Created Servico:\n", docServico);
-    return docServico;
-  });
-};
-
-const addFuncionarioServico = async function (funcionarioId, servicoId) {
-  var doc = await db.FuncionarioServico.create({
-    servicoId: servicoId,
-    funcionarioId: funcionarioId,
-  }).then((doc) => {
-    console.log("\n>> Created FuncionarioServico:\n", doc);
-    return doc;
-  });
-
-  await db.Funcionario.findByIdAndUpdate(funcionarioId, {
-    $push: { servicos: doc._id },
-  });
-  await db.Servico.findByIdAndUpdate(servicoId, {
-    $push: { funcionarios: doc._id },
-  });
-
-  return doc;
-};
-
-const getFuncionarioWithPopulate = function (id) {
-  return db.Funcionario.findById(id)
-    .populate("cartoesChave")
-    .populate({
-      path: "servicos",
-      populate: {
-        path: "servico",
-      },
-    });
-};
-
-const getServicoWithPopulate = function (id) {
-  return db.Servico.findById(id).populate({
-    path: "funcionarios",
-    populate: {
-      path: "funcionario",
-    },
-  });
-};
-
-const createQuarto = function (quarto) {
-  return db.Quarto.create(quarto).then((q) => {
-    console.log("\n>> Created Quarto:\n", q);
-    return q;
-  });
-};
-
-const createCartaoChave = function (id) {
-  return db.CartaoChave.create({ codigo: id }).then((cartao) => {
-    console.log("\n>> Created Cartao:\n", cartao);
-    return cartao;
-  });
-};
-
-const pushCartaoChaveToFunc = function (funcId, cartao) {
-  return db.Funcionario.updateOne(
-    { _id: funcId },
-    { $push: { cartoesChave: cartao } }
-  );
-};
-
-const pullCartaoChaveToFunc = function (funcId, cartao) {
-  return db.Funcionario.updateOne(
-    { _id: funcId },
-    { $pull: { cartoesChave: cartao } }
-  );
-};
-
-const run = async function () {
-  await dropAll();
-
+const addFuncionarios = async function () {
   var func1 = await createFuncionario({
     nome: "Func #1",
     cpf: "1234",
@@ -94,17 +22,6 @@ const run = async function () {
     genero: "M",
     senha: "1234",
   });
-  var servicoA = await createServico({
-    nome: "servicoA",
-  });
-  var servicoB = await createServico({
-    nome: "servicoB",
-  });
-
-  var fs1a = await addFuncionarioServico(func1._id, servicoA._id);
-  console.log("\n>> fs1a:\n", fs1a);
-  var fs1b = await addFuncionarioServico(func1._id, servicoB._id);
-  console.log("\n>> fs1b:\n", fs1b);
 
   var func2 = await createFuncionario({
     nome: "Func #2",
@@ -115,6 +32,21 @@ const run = async function () {
     senha: "1234",
   });
 
+  return [func1, func2];
+};
+
+const addServicos = async function () {
+  var servicoA = await createServico({
+    nome: "servicoA",
+  });
+  var servicoB = await createServico({
+    nome: "servicoB",
+  });
+
+  return [servicoA, servicoB];
+};
+
+const addQuartos = async function () {
   var quarto1 = await createQuarto({
     nome: "Quarto top",
     numero: "25a",
@@ -133,31 +65,42 @@ const run = async function () {
     ocupado: false,
   });
 
+  return [quarto1, quarto2];
+};
+
+const addCartoes = async function () {
   var card1 = await createCartaoChave("12 34 56 78");
   var card2 = await createCartaoChave("87 65 43 21");
 
-  t1 = await pushCartaoChaveToFunc(func1._id, card1);
-  t2 = await pushCartaoChaveToFunc(func2._id, card2);
-
-  // console.log(t1, t2);
-
-  var fs2b = await addFuncionarioServico(func2._id, servicoB._id);
-  console.log("\n>> fs2b:\n", fs2b);
-
-  funcionario = await getFuncionarioWithPopulate(func1._id);
-  console.log("\n>> populated func1:\n", funcionario);
-
-  servico = await getServicoWithPopulate(servicoB._id);
-  console.log("\n>> populated servicoB:\n", servico);
+  return [card1, card2];
 };
 
-const dropCollection = function (collection) {
-  return new Promise((resolve) => {
-    mongoose.connection.collections[collection].drop({}, () => {
-      console.log(`Dropped collection ${collection} successfully!`);
-      resolve(collection);
-    });
-  });
+const run = async function () {
+  await dropAll();
+
+  var func = await addFuncionarios();
+  var serv = await addServicos();
+
+  var fs00 = await addFuncionarioServico(func[0]._id, serv[0]._id);
+  console.log("\n>> fs1a:\n", fs00);
+  var fs01 = await addFuncionarioServico(func[0]._id, serv[1]._id);
+  console.log("\n>> fs1b:\n", fs01);
+
+  var quarto = await addQuartos();
+
+  var card = await addCartoes();
+
+  t1 = await pushCartaoChaveToFunc(func[0]._id, card[0]);
+  t2 = await pushCartaoChaveToFunc(func[1]._id, card[1]);
+
+  var fs11 = await addFuncionarioServico(func[1]._id, serv[1]._id);
+  console.log("\n>> fs2b:\n", fs11);
+
+  funcionario = await getFuncionarioWithPopulate(func[0]._id);
+  console.log("\n>> populated func1:\n", funcionario);
+
+  servico = await getServicoWithPopulate(serv[1]._id);
+  console.log("\n>> populated servicoB:\n", servico);
 };
 
 const dropAll = async function () {
---
+++ scripts/utilsDB.js
@@ -0,0 +1,102 @@
+const createFuncionario = function (funcionario) {
+  return db.Funcionario.create(funcionario).then((docFuncionario) => {
+    console.log("\n>> Created Funcionario:\n", docFuncionario);
+    return docFuncionario;
+  });
+};
+
+const createServico = function (servico) {
+  return db.Servico.create(servico).then((docServico) => {
+    console.log("\n>> Created Servico:\n", docServico);
+    return docServico;
+  });
+};
+
+const addFuncionarioServico = async function (funcionarioId, servicoId) {
+  var doc = await db.FuncionarioServico.create({
+    servicoId: servicoId,
+    funcionarioId: funcionarioId,
+  }).then((doc) => {
+    console.log("\n>> Created FuncionarioServico:\n", doc);
+    return doc;
+  });
+
+  await db.Funcionario.findByIdAndUpdate(funcionarioId, {
+    $push: { servicos: doc._id },
+  });
+  await db.Servico.findByIdAndUpdate(servicoId, {
+    $push: { funcionarios: doc._id },
+  });
+
+  return doc;
+};
+
+const getFuncionarioWithPopulate = function (id) {
+  return db.Funcionario.findById(id)
+    .populate("cartoesChave")
+    .populate({
+      path: "servicos",
+      populate: {
+        path: "servico",
+      },
+    });
+};
+
+const getServicoWithPopulate = function (id) {
+  return db.Servico.findById(id).populate({
+    path: "funcionarios",
+    populate: {
+      path: "funcionario",
+    },
+  });
+};
+
+const createQuarto = function (quarto) {
+  return db.Quarto.create(quarto).then((q) => {
+    console.log("\n>> Created Quarto:\n", q);
+    return q;
+  });
+};
+
+const createCartaoChave = function (id) {
+  return db.CartaoChave.create({ codigo: id }).then((cartao) => {
+    console.log("\n>> Created Cartao:\n", cartao);
+    return cartao;
+  });
+};
+
+const pushCartaoChaveToFunc = function (funcId, cartao) {
+  return db.Funcionario.updateOne(
+    { _id: funcId },
+    { $push: { cartoesChave: cartao } }
+  );
+};
+
+const pullCartaoChaveToFunc = function (funcId, cartao) {
+  return db.Funcionario.updateOne(
+    { _id: funcId },
+    { $pull: { cartoesChave: cartao } }
+  );
+};
+
+const dropCollection = function (collection) {
+    return new Promise((resolve) => {
+      mongoose.connection.collections[collection].drop({}, () => {
+        console.log(`Dropped collection ${collection} successfully!`);
+        resolve(collection);
+      });
+    });
+  };
+
+module.exports = {
+    createFuncionario,
+    createServico,
+    addFuncionarioServico,
+    getFuncionarioWithPopulate,
+    getServicoWithPopulate,
+    createQuarto,
+    createCartaoChave,
+    pushCartaoChaveToFunc,
+    pullCartaoChaveToFunc,
+    dropCollection
+};
